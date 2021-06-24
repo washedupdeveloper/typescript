065d2ce2a9f46eb255db311f5b05f5a72b9fcf87
Storm K <asge0907@easv365.dk>
2021-06-25 01:33:01 +0200

    simplified logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,10 @@
 import pino from "pino";
 
-const prettyPrintConfig = {
-	colorize: true,
-	translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
-	ignore: "pid,hostname",
-};
-
 export const logger = pino({
 	name: process.env["npm_package_name"],
-	prettyPrint: prettyPrintConfig,
+	prettyPrint: {
+		colorize: true,
+		translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+		ignore: "pid,hostname",
+	},
 });
